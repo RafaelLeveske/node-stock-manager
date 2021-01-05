5223376366e587c926e8f00e5ffaada483990eb8
Rafael Leveske <rafaelleveske@gmail.com>
2021-01-05 10:35:00 -0300

    Criação de autenticação

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,17 +1,25 @@
 import 'reflect-metadata';
 
+import * as dotenv from 'dotenv';
+
 import express, { Request, Response, NextFunction } from 'express';
 
-import AppError from '@shared/errors/AppError';
+import cors from 'cors';
 
 import { errors } from 'celebrate';
+import 'express-async-errors';
+import AppError from '@shared/errors/AppError';
+
 import routes from './routes';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
 
+dotenv.config();
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(errors());
